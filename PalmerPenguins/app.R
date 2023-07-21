f0adf388c7a6f74447f042fe3fa023af038c7689
Shefali Chandra <shefali.chandra98@gmail.com>
2023-07-21 22:58:52 +0530

    added formatted tables

--- PalmerPenguins/app.R
+++ PalmerPenguins/app.R
@@ -1,6 +1,8 @@
 #Dashboard for Palmer penguins
 #Author- Shefali C.
-#Start Date- July 17, 2023
+#Last Update- July 21, 2023
+
+##Help- https://clarewest.github.io/blog/post/making-tables-shiny/
 
 #load libraries
 library(tidyverse)
@@ -11,35 +13,3 @@ library(palmerpenguins)
 #data
 penguins <- palmerpenguins::penguins
 
-#User Interface
-ui <- fluidPage(
-  #set title
-  titlePanel(title = "Palmer Penguins"),
-  
-  #Side bar panel
-  sidebarLayout(
-    #side bar layout
-    sidebarPanel(
-      selectInput(inputId = "select_species",
-                  label = "Select species",
-                  choices = list("Adelie", "Gentoo", "Chinstrap"),
-                  selected = "Gentoo")
-    ),#sidebar layout
-    
-    
-    #main panel
-    mainPanel(
-      dataTableOutput(outputId = "dtable")
-    )
-  )
-)
-
-#server
-server <- function(input, output){
-  output$dtable <- renderDataTable(
-    datatable(penguins %>% filter(species == input$select_species))
-  )
-}
-
-#call shiny app
-shinyApp(ui = ui, server = server)
