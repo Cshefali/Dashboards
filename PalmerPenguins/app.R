bb14f53dc940ea7fdc2dc4ea075cf2a2e081eb07
Shefali Chandra <shefali.chandra98@gmail.com>
2023-07-28 14:33:01 +0530

    updated dom with 2 tables

--- PalmerPenguins/app.R
+++ PalmerPenguins/app.R
@@ -23,6 +23,15 @@ library(shinyjqui)
 penguins <- palmerpenguins::penguins
 colnames(penguins) <- gsub(pattern = "_mm",replacement = "",colnames(penguins))
 
+#CSS for globally centering the search box
+css <- "
+div.dataTables_wrapper  div.dataTables_filter {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+"
+
 #User INterface
 # ui <- basicPage(
 #   h3("Palmer Penguins table using DT and Formattable packages"),
@@ -41,6 +50,9 @@ colnames(penguins) <- gsub(pattern = "_mm",replacement = "",colnames(penguins))
 # )
 
 ui <- fluidPage(
+      # tags$head(
+      #   tags$style(HTML(css))
+      # ),
   #title panel
   titlePanel(title = "Table Styling"),
   #change color of selected row to pink
@@ -50,8 +62,19 @@ ui <- fluidPage(
     sidebarPanel(),
     mainPanel(
       h3("Palmer Penguins table using DT and Formattable packages"),
+      #br(),
       div(DT::dataTableOutput(outputId = "group_view"), 
-          style = "font-size: 75%; width = 75%")
+          style = "font-size: 75%; width = 75%"),
+      h3("Second table"),
+      br(),
+      tags$style(HTML("table2 > .dataTables_wrapper > .dataTables_filter {
+                        position: absolute;
+                        left: 50%;
+                        transform: translateX(-50%);
+                      }")),
+      div(DT::DTOutput(outputId = "table2"),
+          style = "font-size: 75%; width = 75%"),
+      
     )
   )
 )
@@ -95,7 +118,7 @@ server <- function(input, output){
                 #mis-aligned column header and table body
                 autoWidth = FALSE,
                 server = TRUE,
-                dom = 'lfrtBip',
+                dom = 'lftBip',
                 #dom = 'Bfrtip',
                 #change seach box title from "Search" to "Filter"
                 language = list(search = 'Filter:'),
@@ -150,6 +173,101 @@ server <- function(input, output){
       #formatStyle(fontWeight = "bold")
       #formatCurrency(columns = 'body_mass_g')
   })#output$dtable
+  
+  #TABLE 2
+  output$table2 <- DT::renderDataTable({
+    datatable(data = penguins,
+              #class = 'cell-border stripe',
+              #class = 'row-border',
+              class = 'compact row-border stripe cell-border hover',
+              #class = 'compact row-border stripe hover',
+              options = list(
+                #function to change color of the column header
+                initComplete = JS(
+                  "function(settings, json) {",
+                  "$(this.api().table().header()).css({'background-color': '#fff', 'color': '#000'});",
+                  "}"),
+                #change bill_depth row to currency format.
+                rowCallback = JS(
+                  "function(row, data) {",
+                  "var num = '$' + data[5].toString().replace(/\\B(?=(\\d{5})+(?!\\d))/g, ',');",
+                  "$('td:eq(5)', row).html(num);",
+                  "}"),
+                #change font to georgia--NOt working
+                initComplete = JS(
+                  "function(settings, json) {",
+                  "$('body').css({'font-family': 'Georgia'});",
+                  "}"
+                ),
+                #function for column reordering
+                #callback = JS(js),
+                paging = TRUE,
+                #default length of each page
+                pageLength = 10,
+                #option for number of rows to display- 5, 10, 15 or 20 rows.
+                lengthMenu = c(5,10,15,20),
+                scrollX = TRUE,
+                #scrollY is not a boolean argument. specify the fixed height of table
+                scrollY = "400px",
+                #keeping autowidth false solves the problem of 
+                #mis-aligned column header and table body
+                autoWidth = FALSE,
+                server = TRUE,
+                dom = 'lftBip',
+                #dom = 'Bfrtip',
+                #change seach box title from "Search" to "Filter"
+                language = list(search = 'Filter:'),
+                buttons = list('print',
+                               #Download button with 3 format options
+                               list(extend = 'collection',
+                                    buttons = c('csv','excel','pdf'),
+                                    text = 'Download'),
+                               #button for column visibility; columns 1 and 2 fixed
+                               list(extend = 'colvis',
+                                    columns = 2:7)),
+                
+                #order the table in descending order of bill length
+                order = list(list(6,'desc')),
+                #highlight search results
+                searchHighlight = TRUE,
+                #user can reorder the columns
+                ColReorder = TRUE,
+                #fixing some columns (another argument--rightColumns = xx)
+                fixedColumns = list(leftColumns = 2),
+                #fix the column headers when scrolling down
+                fixedHeader = TRUE,
+                #enable moving around cells using keys
+                keys = TRUE,
+                #Remove the sorting arrows--Does not work, both
+                #ordering = F,
+                #bSort = F,
+                #create a scroller for large tables
+                #deferRender = TRUE,
+                #scroller = TRUE,
+                #applying some column options, make all columns center aligned
+                columnDefs = list(
+                  #center adjust values in all columns
+                  list(targets = '_all', className = 'dt-center'),
+                  #set width of columns 3 to 6.
+                  list(targets = c(3,4,5,6), width = "30px"))
+                #list(targets = c(0, 8, 9), visible = FALSE))
+              ),#options
+              callback = JS("table.order([5, 'desc']).draw();"),
+              #adds 'csv' and 'excel' download buttons.
+              #Responsive collapses the columns when window size reduced.
+              extensions = c('Buttons','ColReorder','FixedHeader','KeyTable',
+                             'Responsive'),
+              #adds capability of click & select any row; default- 'multiple'
+              selection = 'single',
+              #add/remove a search box below/above ('top', 'bottom') each column of table
+              filter = 'none',
+              #removes the row index or name, if any from LHS
+              rownames = FALSE
+    )   #datatable
+    ##below fomratStyles do not work!!
+    #formatStyle(fontWeight = "bold")
+    #formatCurrency(columns = 'body_mass_g')
+  })#output$table2
   
 }#server
 
