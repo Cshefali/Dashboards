63cc2e893650be4894ca358272cd9625178b6be0
Shefali Chandra <shefali.chandra98@gmail.com>
2023-08-03 02:53:16 +0530

    updated fluidrow

--- Tutorial/application-layouts/complex_fluidRow.R
+++ Tutorial/application-layouts/complex_fluidRow.R
@@ -10,3 +10,47 @@ library(dplyr)
 library(ggplot2)
 
 data1 <- diamonds
+
+#User interface
+ui <- fluidPage(
+  #titlePanel(title = "Diamonds Dataset"),
+  
+    #facet grid plot
+    plotOutput(outputId = "diamond"),
+    #fluidRow section
+    fluidRow(
+      column(3,
+             h4(strong("Diamonds Explorer")),
+             
+             sliderInput(inputId = "sample_size",
+                         label = "Sample Size",
+                         min = 1,
+                         max = nrow(diamonds),
+                         value = 1000,
+                         step = 500),
+             
+             #checkbox for Jitter plot
+             checkboxInput(inputId = "jitter_plot",
+                           label = "Jitter",
+                           value = F),
+             
+             #checkbox for Smooth
+             checkboxInput(inputId = "smooth_plot",
+                           label = "Smooth",
+                           value = F)
+             ),#column 1
+      column(4, 
+             selectInput(inputId = "var1", label = "X",
+                         label = "X",
+                         choices = colnames(diamonds)),
+             selectInput(inputId = "var2", label = "Y",
+                         choices = colnames(diamonds), selected = "cut"),
+             selectInput(inputId = "color", label = "Color",
+                         selected = c('None',names(diamonds)))
+             ),#column 2
+      column(4,
+             selectInput())
+    )#fluidRow
+    
+  
+)
